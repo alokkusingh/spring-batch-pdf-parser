7bc613219b02606ae71c9884ca18dc43ce3485cc
alokkusingh <alok.ku.singh@gmail.com>
2022-02-10 11:05:05 +0530

    Added transaction AVIN regexp

--- src/main/java/com/alok/spring/batch/utils/Utility.java
+++ src/main/java/com/alok/spring/batch/utils/Utility.java
@@ -12,7 +12,7 @@ public class Utility {
     }
 
     static public boolean isFamilyTransaction(String transation) {
-        if (transation.toLowerCase().matches(".*ramawatar.*|.*avinash.*|.*AVIN XX7201.*|.*gopal.*|.*papa.*|.*31987667084.*" +
+        if (transation.toLowerCase().matches(".*ramawatar.*|.*avinash.*|.*avin.*|.*gopal.*|.*papa.*|.*31987667084.*" +
                 "|.*3209010000019.*|.*kharagpur.*|.*mb hr.*|.*kumari  jyoti.*|.*pankaj  kumar.*|.*bihar.*" +
                 "|.*yogendra  narayan.*|.*shailendra  singh.*|.*manju  devi.*|.*vivekanand  singh.*")) {
             if (!transation.toLowerCase().matches(".*rachna.*|.*withdrawal.*|.*9916661247@.*"
