3f2daea2be334ec950574a5867d76c86b3be4860
alokkusingh <alok.ku.singh@gmail.com>
2022-02-18 10:56:02 +0530

    Phase 2 - Changes

--- src/main/java/com/alok/spring/batch/controller/ReportController.java
+++ src/main/java/com/alok/spring/batch/controller/ReportController.java
@@ -2,6 +2,7 @@ package com.alok.spring.batch.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -11,8 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Slf4j
 @RestController
@@ -23,8 +29,12 @@ public class ReportController {
     private String googleCsvFile;
 
     @GetMapping("/download")
-    public ResponseEntity<Resource> downloadReport() throws FileNotFoundException {
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(googleCsvFile));
+    public ResponseEntity<Resource> downloadReport() throws IOException {
+        //InputStreamResource resource = new InputStreamResource(new FileInputStream(googleCsvFile));
+        File file = new File(googleCsvFile);
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
