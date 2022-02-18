bf7882d9ce0656c18d73850b89c093655c9a5fba
alokkusingh <alok.ku.singh@gmail.com>
2022-02-18 13:17:26 +0530

    Phase 2 - Changes

--- src/main/java/com/alok/spring/batch/controller/BankStatementController.java
+++ src/main/java/com/alok/spring/batch/controller/BankStatementController.java
@@ -2,7 +2,6 @@ package com.alok.spring.batch.controller;
 
 import com.alok.spring.batch.response.UploadFileResponse;
 import com.alok.spring.batch.service.FileStorageService;
-import com.sun.xml.internal.ws.api.pipe.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
