2ed7a3d12ea887e439fc7a6ab11ee7564b6966ed
alokkusingh <alok.ku.singh@gmail.com>
2022-03-31 10:30:14 +0530

    Bug fixing

--- src/main/java/com/alok/spring/batch/repository/ProcessedFileRepository.java
+++ src/main/java/com/alok/spring/batch/repository/ProcessedFileRepository.java
@@ -2,11 +2,13 @@ package com.alok.spring.batch.repository;
 
 import com.alok.spring.batch.model.ProcessedFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProcessedFileRepository extends JpaRepository<ProcessedFile, Integer> {
     Optional<List<ProcessedFile>> findAllByName(String valueOf);
+    @Query("DELETE from  ProcessedFile pf where pf.type = ?1")
     void deleteAllByType(String type);
 }
