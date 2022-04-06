fb5cf139fc370a18bb26d8c139dfb03701a0c83d
alokkusingh <alok.ku.singh@gmail.com>
2022-04-06 11:23:38 +0530

    Handling datetime

--- src/main/java/com/alok/spring/batch/processor/DefaultExpenseProcessor.java
+++ src/main/java/com/alok/spring/batch/processor/DefaultExpenseProcessor.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 @Component("defaultExpenseProcessor")
 @Slf4j
@@ -31,11 +32,23 @@ public class DefaultExpenseProcessor implements ItemProcessor<Expense, Expense> 
     public Expense process(Expense transaction) throws ParseException {
         setExpenseCategory(transaction);
 
+        // the date input in the csv is in IST format but ??? utilis think the date is in UTC
+        // to correct this deducting 05:30 hours for IST time
+        fixDate(transaction);
+
         return transaction;
     }
 
     protected void setExpenseCategory(Expense transaction) {
         transaction.setCategory(Utility.getExpenseCategory(transaction.getHead(), transaction.getComment()));
+    }
+
+    protected void fixDate(Expense transaction) {
+        Date oldDate = transaction.getDate();
+
+        long istTime = oldDate.getTime() - 19800000;
+        Date newDate = new Date(istTime);
+        transaction.setDate(newDate);
     }
 
 
